1e05567acf3ecad5bdef9b52f9fbe93ac5a7f9ee
Aram <arma-arman@mail.ru>
2022-08-07 10:19:29 +0400

    commited JS and HTML files

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,7 @@
-document.body.innerHTML = "Hello Git";
+const canvas = document.querySelector("canvas");
+const input = document.querySelector("input");
+const ctx = canvas.getContext("2d");
+
+canvas.addEventListener("click", function() {
+    ctx.fillRect(0, 0, input.value, input.value); 
+});
